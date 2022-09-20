3ce3e2352247ab721e83a6c98149c4eea8733ef0
unknown <akshay23lonare@gmail.com>
2022-09-20 23:46:35 +0530

    code done by ak

--- prime.js
+++ prime.js
@@ -9,3 +9,14 @@ let counter=0;
     if(counter==2){
        return true
     }
+     return false
+
+}
+
+let ans=prime(7);
+if (ans==true){
+ console.log("Prime");
+}
+else{
+   console.log("Not Prime")
+}
